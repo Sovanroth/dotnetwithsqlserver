e895c05684ac7c1219b8a2ee342ec3b8a11b7251
Sovanroth13 <sovanroth016@gmail.com>
2024-05-22 11:42:38 +0700

    init v1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dotnetwithsqlserver.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
